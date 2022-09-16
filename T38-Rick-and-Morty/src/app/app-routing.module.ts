5c471185635ab731d13905419360e8c5bf57a5fa
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-09-16 17:37:42 +0200

    Update and add components and services

--- T38-Rick-and-Morty/src/app/app-routing.module.ts
+++ T38-Rick-and-Morty/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { CharactersComponent } from './characters/characters.component';
 import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
 import { AddCharacterComponent } from "./add-character/add-character.component";
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 
 const routes: Routes = [
 
@@ -23,7 +28,22 @@ const routes: Routes = [
     }, {
         path: 'add',
         component: AddCharacterComponent
-    }
+    }, {
+        path: 'login',
+        component: LoginComponent
+    }, {
+        path: 'profile',
+        component: ProfileComponent
+    }, {
+        path: 'user',
+        component: BoardUserComponent
+    }, {
+        path: 'mod',
+        component: BoardModeratorComponent
+    }, {
+        path: 'admin',
+        component: BoardAdminComponent
+}
 
 ];
 
--- T38-Rick-and-Morty/src/app/app.module.ts
+++ T38-Rick-and-Morty/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { InfoComponent } from './info/info.component';
 import { AddCharacterComponent } from './add-character/add-character.component';
 import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from "ngx-pagination";
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { AuthinterceptorComponent } from './_helpers/authinterceptor/authinterceptor.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,13 @@ import { NgxPaginationModule } from "ngx-pagination";
     CharactersComponent,
     AboutComponent,
     InfoComponent,
-    AddCharacterComponent
+    AddCharacterComponent,
+    LoginComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+    AuthinterceptorComponent
   ],
   imports: [
     BrowserModule,
